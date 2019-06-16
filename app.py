de3bb959f9cbb0f930ee3ebed427a96253d3775b
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2019-06-16 13:57:08 +0100

    adding recipes function added and wired up to the database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
@@ -11,10 +12,33 @@ app.config['MONGO_DBNAME']='recipes_database'
 
 mongo = PyMongo(app)
 recipes_collection = mongo.db.recipes
+users_collection = mongo.db.users
 
 
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
   return render_template("recipes.html", recipes=recipes_collection.find())
+
+@app.route('/add_recipe', methods=['GET', 'POST'])
+def add_recipe():
+  if request.method == 'POST':
+    form = request.form.to_dict()
+    recipes = recipes_collection
+    recipes.insert_one(
+      {
+        'name': form['name'],
+        'description': form['description'],
+        'author': form['author'],
+        'cuisine': form['cuisine'],
+        'allergens': form['allergens'],
+        'ingredients': (form['ingredients'].split(',')),
+        'preparation': form['preparation'],
+        'vegetarian': bool(form['vegetarian']),
+        'like': bool('false'),
+        'votes': int('0'),
+        'date_added': datetime.datetime.now()
+      }
+    )
+  return render_template("addrecipe.html")
 
